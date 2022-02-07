577b78bdbd9017c6d2e3fa2d298a6ad36ef26948
Simon <simonbesteiro@gmail.com>
2022-02-07 16:24:05 +0100

    create accpetance test

---
+++ src/test/java/kata/BankAcceptanceTest.java
@@ -0,0 +1,33 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAcceptanceTest {
+    /*
+Given a client makes a deposit of 1000 on 10-01-2012
+And a deposit of 2000 on 13-01-2012
+And a withdrawal of 500 on 14-01-2012
+When they print their bank statement
+Then they would see:
+
+Date       || Amount || Balance
+14/01/2012 || -500   || 2500
+13/01/2012 || 2000   || 3000
+10/01/2012 || 1000   || 1000
+     */
+    @Test void
+    shouldPrintDesiredTransactions(){
+        String expectedOutput= "Date       || Amount || Balance\n" +
+            "14/01/2012 || -500   || 2500\n" +
+            "13/01/2012 || 2000   || 3000\n" +
+            "10/01/2012 || 1000   || 1000";
+        var account = new Account();
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+        var result= account.print();
+        assertEquals(expectedOutput, result);
+    }
+}
