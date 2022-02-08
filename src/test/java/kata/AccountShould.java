3d7118155866cd2297adb867156bb0c1094a3435
Simon <simonbesteiro@gmail.com>
2022-02-08 16:07:58 +0100

    red

--- src/test/java/kata/AccountShould.java
+++ src/test/java/kata/AccountShould.java
@@ -1,8 +1,15 @@
 package kata;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class AccountShould {
 
@@ -22,5 +29,25 @@ public class AccountShould {
         var account = new Account(transactionRepository);
         account.withdraw(10);
         verify(transactionRepository).add(new Money(-10));
+    }
+
+    @Test
+    void
+    should_print_a_transaction() throws ParseException {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        String expectedOutput= "Date || Amount || Balance\n" +
+            "10/01/2012 || 10 || 10";
+
+        DateProviderService dateProvider = mock(DateProviderService.class);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date expectedDate = formatter.parse("2012-01-10");
+        when(dateProvider.getNow()).thenReturn(expectedDate);
+
+        var account = new Account(new MemoryTransactionRepository(new DateProvider()));
+        account.deposit(10);
+        account.print();
+        assertEquals(expectedOutput, output.toString());
     }
 }
